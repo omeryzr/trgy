f9965886f156348b19c18bb9c9abf06032c7e8e8
omeryzr <omeryzr_@hotmail.com>
2016-12-10 17:42:51 +0200

    add files

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$baseUrl = 'http://socialogin.herokuapp.com/'; // Projenin bulunduğu dizin yapısı. Örn: http://localhost/twitterApi/
+
+$consumerKey ='NO1Cq2gZp2iFftrsAFKh3YAik'; // Api Key
+$consumerSecret ='BtQUruyX0GcqMMwcQnAVjh1FvbNRc2mRXVi67fEp0DQZDeIVFA'; // Api Secret
+$oauthCallback = 'http://socialogin.herokuapp.com/callback.php'; // Giriş yapıldığında geri döneceği sayfa Örn : http://localhost/twitterApi/callback.php
