c6ad0ae96555307feef765046918a56d9c1793e1
omeryzr <omeryzr_@hotmail.com>
2017-01-05 01:04:29 +0200

    created mytweets pages

--- mytweets.php
+++ mytweets.php
@@ -4,20 +4,21 @@ include "layout/header.php";
 require "vendor/TwitterOAuth/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
+if (!isset($_SESSION['access_token'])) {
+    include 'login.php';
+}
+else
+{
+    $accessToken = $_SESSION['access_token'];
+    $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
+    $connectionOauth->setTimeouts(30, 30);
+
+    
+    $tweets = $connectionOauth->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$username.'&count=2")
+    print_r( $tweets );
 
 
-$accessToken = $_SESSION['access_token'];
-$connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
-$connectionOauth->setTimeouts(30, 30);
 
+}
 
-$username = 'omryazir';
-
-
-$tweets = $connectionOauth->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$username.'&count=2")
-
-foreach ($tweets->users as $tweet) {
-    echo "<h3>$text</h3>";
-  }
-
-  include "layout/footer.php";
+include "layout/footer.php";
