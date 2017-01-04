889a989d5479c22802fc1bcd82b95128b5a36e0e
omeryzr <omeryzr_@hotmail.com>
2017-01-04 17:25:35 +0200

    created new page

---
+++ kullanim.php
@@ -0,0 +1,9 @@
+<?php
+include "layout/header.php";
+echo "<div class='jumbotron'>
+        <h1>SociaLogin Nasıl Kullanılır ?</h1>
+        <p>SociaLogin uygulaması ile twitter hesabınız veya hesaplarınıza bir çok yönde etki edebilirsiniz. Uygulamaya girerken şifre gereği duymadan özelliklerden faydalanabilirsiniz.
+        <br><center><a href = '$url'> <img src='images/signIn.png' style='width: 300px'></center></a></p>
+      </div>";
+
+include "layout/footer.php";
