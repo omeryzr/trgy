420fff73aa262e8fcc9a435e33816286bccde06e
omeryzr <omeryzr_@hotmail.com>
2017-01-04 21:58:13 +0200

    edit pages

--- nedir.php
+++ nedir.php
@@ -1,8 +1,6 @@
 <?php
 include "layout/header.php";
 
-require "vendor/TwitterOAuth/autoload.php";
-use Abraham\TwitterOAuth\TwitterOAuth;
 echo "<div class='jumbotron'>
         <h1>SociaLogin Nedir ?</h1>
         <p>SociaLogin uygulaması ile twitter hesabınız veya hesaplarınıza bir çok yönde etki edebilirsiniz. Uygulamaya girerken şifre gereği duymadan özelliklerden faydalanabilirsiniz.
