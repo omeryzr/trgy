611a77bd2f518fb8c074dfbfff3d2beb0b949238
omeryzr <omeryzr_@hotmail.com>
2017-01-05 01:25:07 +0200

    edit mytweets pages

--- mytweets.php
+++ mytweets.php
@@ -7,7 +7,7 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 
     $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
 
-    $tweets = $connectionOauth->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2");
+    $tweets = $connectionOauth->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=omryazir&count=2");
     print_r( $tweets );
 
 
