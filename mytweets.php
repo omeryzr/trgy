d997a47855eb7331efd1be264a7bb535cf55009e
omeryzr <omeryzr_@hotmail.com>
2017-01-05 01:08:41 +0200

    created mytweets pages

--- mytweets.php
+++ mytweets.php
@@ -4,21 +4,13 @@ include "layout/header.php";
 require "vendor/TwitterOAuth/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-if (!isset($_SESSION['access_token'])) {
-    include 'login.php';
-}
-else
-{
-    $accessToken = $_SESSION['access_token'];
+
     $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
     $connectionOauth->setTimeouts(30, 30);
 
-    
-    $tweets = $connectionOauth->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$username.'&count=2")
+    $username = 'omryazir';
+    $tweets = $connectionOauth->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$username.'&count=2");
     print_r( $tweets );
 
-
-
-}
 
 include "layout/footer.php";
