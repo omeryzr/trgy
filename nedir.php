36077e315baffe0bf10bbfaf2e00247ce37d5590
omeryzr <omeryzr_@hotmail.com>
2017-01-04 21:15:35 +0200

    edit page

--- nedir.php
+++ nedir.php
@@ -1,12 +1,13 @@
 <?php
 include "layout/header.php";
-    include 'login.php';
-    echo "<div class='jumbotron'>
-            <h1>SociaLogin Nedir ?</h1>
-            <p>SociaLogin uygulaması ile twitter hesabınız veya hesaplarınıza bir çok yönde etki edebilirsiniz. Uygulamaya girerken şifre gereği duymadan özelliklerden faydalanabilirsiniz.
-            <br><center><a href = '$url'> <img src='images/signIn.png' style='width: 300px'></center></a></p>
-          </div>";
-}
 
+require "vendor/TwitterOAuth/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+echo "<div class='jumbotron'>
+        <h1>SociaLogin Nedir ?</h1>
+        <p>SociaLogin uygulaması ile twitter hesabınız veya hesaplarınıza bir çok yönde etki edebilirsiniz. Uygulamaya girerken şifre gereği duymadan özelliklerden faydalanabilirsiniz.
+        <br><center><a href = '$url'> <img src='images/signIn.png' style='width: 300px'></center></a></p>
+      </div>";
 
 include "layout/footer.php";
