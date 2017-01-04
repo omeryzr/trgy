4d15ae5452e3a9411489313987d7deb9d97c0e4a
omeryzr <omeryzr_@hotmail.com>
2017-01-05 00:45:33 +0200

    created mytweets pages

--- mytweets.php
+++ mytweets.php
@@ -1,22 +1,23 @@
 <?php
-session_start();
-include "config.php";
+include "layout/header.php";
 
 require "vendor/TwitterOAuth/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-$twitter = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
+
+
+$accessToken = $_SESSION['access_token'];
+$connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
+$connectionOauth->setTimeouts(30, 30);
+
 
 $username = 'omryazir';
 
 
 $tweets = $twitter->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$username.'&count=2")
 
-foreach ($tweets as $tweet ) {
-  $id = $tweet->id_str;
-  $text = $tweet->text;
-  $created_at = date("Y-m-d H:i:s", strtotime($tweet->created_at));
-  echo "<a href='https://twitter.com/'.$username.'/statuses/'.$id.'>'.$text.'<br />
-                    '.$created_at.' -#'.$id.'
-  </a>";
-}
+foreach ($tweets->users as $tweet) {
+    echo "<h3>$text</h3>";
+  }
+
+  include "layout/footer.php";
