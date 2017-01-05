48b311907ecfe703bf8da5172c152608beff620a
omeryzr <omeryzr_@hotmail.com>
2017-01-05 14:54:18 +0200

    edit mytweets

--- mytweets.php
+++ mytweets.php
@@ -1,14 +1,41 @@
 <?php
-include "layout/header.php";
 
-require "vendor/TwitterOAuth/autoload.php";
-use Abraham\TwitterOAuth\TwitterOAuth;
+	header('Content-type: text/html; charset=utf8');
 
+  include "layout/header.php";
 
-    $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
-    $username = 'siIaderki';
-    $tweets = $connectionOauth->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$username.'&count=10");
-    print_r( $tweets );
+  require "vendor/TwitterOAuth/autoload.php";
+  use Abraham\TwitterOAuth\TwitterOAuth;
 
+	// consumer ve access
+	$consumer_key = 'NO1Cq2gZp2iFftrsAFKh3YAik';
+	$consumer_secret = 'BtQUruyX0GcqMMwcQnAVjh1FvbNRc2mRXVi67fEp0DQZDeIVFA';
+	$access_token = '3329469442-P50kd40f7BRT66NErXaoBjbPalQJldsT5HORTsR';
+	$access_token_secret = 'r84D7FTBRySlvraboNOjUJGnieS3retJ0KqZeKDi8EBzV';
+
+	// sıfını başlatalım
+	$twitter = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+
+	// tw - kullanıcı adı
+	$username = 'omryazir';
+
+	// tw sayısı
+	$count = 5;
+
+	$tweets = $twitter->get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$username.'&count='.$count);
+
+	foreach ( $tweets as $tweet ){
+
+		$id = $tweet->id_str;
+		$text = $tweet->text;
+		$created_at = date("Y-m-d H:i:s", strtotime($tweet->created_at));
+
+		echo '<a href="https://twitter.com/'.$username.'/statuses/'.$id.'" target="_blank">
+			'.nl2br($text).'<br />
+			'.$created_at.' - #'.$id.'
+		</a>
+		<hr />';
+
+	}
 
 include "layout/footer.php";
