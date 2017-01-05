263b02c1977767446a69b487d42784ca48212a8e
omeryzr <omeryzr_@hotmail.com>
2017-01-05 15:03:35 +0200

    edit mytweets

--- mytweets.php
+++ mytweets.php
@@ -17,25 +17,13 @@
 	$twitter = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
 
 	// tw - kullanıcı adı
-	$username = 'omryazir';
+	$username = 'siIaderki';
 
 	// tw sayısı
 	$count = 5;
 
 	$tweets = $twitter->get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name='.$username.'&count='.$count);
 
-	foreach ( $tweets as $tweet ){
-
-		$id = $tweet->id_str;
-		$text = $tweet->text;
-		$created_at = date("Y-m-d H:i:s", strtotime($tweet->created_at));
-
-		echo '<a href="https://twitter.com/'.$username.'/statuses/'.$id.'" target="_blank">
-			'.nl2br($text).'<br />
-			'.$created_at.' - #'.$id.'
-		</a>
-		<hr />';
-
-	}
+	print_r ($tweets);
 
 include "layout/footer.php";
