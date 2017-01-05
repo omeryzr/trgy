74df3809b7c2df6038581fce4dea31626222516e
omeryzr <omeryzr_@hotmail.com>
2017-01-05 22:55:53 +0200

    edit tweets page

--- tweets.php
+++ tweets.php
@@ -4,12 +4,6 @@
   require "vendor/TwitterOAuth/autoload.php";
   use Abraham\TwitterOAuth\TwitterOAuth;
 
-	// consumer ve access
-	$consumer_key = 'NO1Cq2gZp2iFftrsAFKh3YAik';
-	$consumer_secret = 'BtQUruyX0GcqMMwcQnAVjh1FvbNRc2mRXVi67fEp0DQZDeIVFA';
-	$access_token = '3329469442-P50kd40f7BRT66NErXaoBjbPalQJldsT5HORTsR';
-	$access_token_secret = 'r84D7FTBRySlvraboNOjUJGnieS3retJ0KqZeKDi8EBzV';
-
 	// sıfını başlatalım
 	$twitter = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
 
