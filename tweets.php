8da52bd94a73827e61ef95740457cc9163dd921c
omeryzr <omeryzr_@hotmail.com>
2017-01-06 20:29:51 +0200

    edit

--- tweets.php
+++ tweets.php
@@ -7,14 +7,9 @@
   $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
   $tweet->setTimeouts(30, 30);
 
-  function podpowiedzi($tweet, $q = "muzyka"){
+  $q = "muzyka";
 	$users = $tweet->get('users/search', array('q' => $q));
-	$a = json_decode($users, true);
-	echo "<pre>";
-	//print_r($a);
-	foreach ($a as $key => $user) {
-		echo $user['screen_name']." ".$user['id']." Follow user <br>";
-		$ret = $tweet->post('friendships/create', array('user_id' => $user['id']));
-	}
-}
+  print_r($users);
+
+
   include "layout/footer.php";
