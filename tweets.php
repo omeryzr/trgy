b721a18c38df86a56da6a89450ba44460b3bffd2
omeryzr <omeryzr_@hotmail.com>
2017-01-06 17:10:58 +0200

    edit

--- tweets.php
+++ tweets.php
@@ -6,11 +6,15 @@
 
   $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
   $connectionOauth->setTimeouts(30, 30);
-  $query='ankara';
-  $tweets->get('search/tweets.json', [
-                                'query' => ['q' => $query]
-                        ]);
 
-  print_r($tweets);
+  $url = 'https://api.twitter.com/1.1/search/tweets.json';
+$requestMethod = 'GET';
+$getfield = '?q=#baseball&result_type=recent';
+
+// Perform the request
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest();
 
   include "layout/footer.php";
