de233b01206e60bc69769683e8fc26d78116c80a
omeryzr <omeryzr_@hotmail.com>
2017-01-06 16:45:38 +0200

    edit

--- tweets.php
+++ tweets.php
@@ -7,7 +7,9 @@
   $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
   $connectionOauth->setTimeouts(30, 30);
 
-  $tweets = $connectionOauth->get("search/tweets" , array('count' => 20));
+  $tweets->get('search/tweets.json', [
+                                'query' => ['q' => $query]
+                        ]);
 
   print_r($tweets);
 
