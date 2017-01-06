b967b02251f6cc291399f4f5e1b7c843ba2320aa
omeryzr <omeryzr_@hotmail.com>
2017-01-06 20:15:27 +0200

    edit

--- tweets.php
+++ tweets.php
@@ -4,16 +4,17 @@
   require "vendor/TwitterOAuth/autoload.php";
   use Abraham\TwitterOAuth\TwitterOAuth;
 
-  $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
-  $connectionOauth->setTimeouts(30, 30);
+  $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
+  $tweet->setTimeouts(30, 30);
 
-  $url = 'https://api.twitter.com/1.1/search/tweets.json';
-  $requestMethod = 'GET';
-  $getfield = '?q=#baseball&result_type=recent';
-
-// Perform the request
-  $twitter = new TwitterAPIExchange($settings);
-  echo $twitter->setGetfield($getfield)
-             ->buildOauth($url, $requestMethod)
-             ->performRequest();
+  function search($tweet, $q = "tv"){
+	$users = $tweet->get('search/tweets', array('q' => $q, 'lang' => "pl", 'result_type' => 'popular'));
+	$a = json_decode($users, true);
+	echo "<pre>";
+	print_r($a);
+	foreach ($a as $key => $user) {
+		//echo $user['screen_name']." ".$user['id']." Follow user <br>";
+		//$ret = $tweet->post('friendships/create', array('user_id' => $user['id']));
+	}
+}
   include "layout/footer.php";
