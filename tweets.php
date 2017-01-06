ca73ff89ebb1f0c01b972c5afb86b4ee05497276
omeryzr <omeryzr_@hotmail.com>
2017-01-06 20:58:05 +0200

    edit

--- tweets.php
+++ tweets.php
@@ -1,21 +1,25 @@
-<?php
+  <?php
   include "layout/header.php";
 
   require "vendor/TwitterOAuth/autoload.php";
   use Abraham\TwitterOAuth\TwitterOAuth;
 
-  $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
-  $connectionOauth->setTimeouts(30, 30);
+  if (!isset($_SESSION['access_token'])) {
+      include 'login.php';
+  }
+  else
+  {
+      $accessToken = $_SESSION['access_token'];
+      $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
+      $connectionOauth->setTimeouts(30, 30);
+      $q = "muzyka";
+      $users = $connectionOauth->get('users/search', array('q' => $q));
+      $a = json_decode($users, true);
+      print_r($a);
 
-  function podpowiedzi($connectionOauth, $q = "muzyka"){
-	$users = $connectionOauth->get('users/search', array('q' => $q));
-	$a = json_decode($users, true);
-	echo "<pre>";
-	print_r($a);
-	// foreach ($a as $key => $user) {
-	// 	echo $user['screen_name']." ".$user['id']." Follow user <br>";
-	// 	$ret = $connectionOauth->post('friendships/create', array('user_id' => $user['id']));
-	// }
-}
+
+
+
+  }
 
   include "layout/footer.php";
