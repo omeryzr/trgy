c38a5fd68f75b936bc40de287fc5d81c12b27105
omeryzr <omeryzr_@hotmail.com>
2017-01-06 20:34:38 +0200

    edit

--- tweets.php
+++ tweets.php
@@ -4,11 +4,11 @@
   require "vendor/TwitterOAuth/autoload.php";
   use Abraham\TwitterOAuth\TwitterOAuth;
 
-  $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
-  $tweet->setTimeouts(30, 30);
+  $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
+  $connectionOauth->setTimeouts(30, 30);
 
   $q = "muzyka";
-	$users = $tweet->get('users/search', array('q' => $q));
+	$users = $connectionOauth->get('users/search', array('q' => $q));
   print_r($users);
 
 
