10a6295769cdc02c74817421ec6e4c105d95f446
omeryzr <omeryzr_@hotmail.com>
2017-01-06 20:53:18 +0200

    edit

--- tweets.php
+++ tweets.php
@@ -7,12 +7,15 @@
   $connectionOauth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken['oauth_token'], $accessToken['oauth_token_secret']);
   $connectionOauth->setTimeouts(30, 30);
 
-  $q = "muzyka";
+  function podpowiedzi($connectionOauth, $q = "muzyka"){
 	$users = $connectionOauth->get('users/search', array('q' => $q));
-
-  foreach ( $users as $user ){
-  $name = $user->name;
-  echo "$id";
-  }
+	$a = json_decode($users, true);
+	echo "<pre>";
+	//print_r($a);
+	foreach ($a as $key => $user) {
+		echo $user['screen_name']." ".$user['id']." Follow user <br>";
+		$ret = $connectionOauth->post('friendships/create', array('user_id' => $user['id']));
+	}
+}
 
   include "layout/footer.php";
